8e4badf845e1b5651cef8f34ca6e926f63d4cdee
LuisJose <luisjoserranoef2014@gmail.com>
2023-10-10 17:18:02 +0200

    Interfaz gráfica hecha

--- Encuesta/app/src/main/java/com/example/encuesta/MainActivity.kt
+++ Encuesta/app/src/main/java/com/example/encuesta/MainActivity.kt
@@ -1,11 +1,31 @@
-<package com.example.encuesta
+package com.example.encuesta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import com.example.encuesta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sbSeekbar.setOnSeekBarChangeListener(object:
+            SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                var progreso = binding.sbSeekbar.progress
+                binding.txtHoras.setText(progreso.toString())
+            }
+            override fun onStartTrackingTouch(seek: SeekBar) {
+            }
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                var progreso = binding.sbSeekbar.progress
+                binding.txtHoras.setText(progreso.toString())
+            }
+        })
+
     }
 }
