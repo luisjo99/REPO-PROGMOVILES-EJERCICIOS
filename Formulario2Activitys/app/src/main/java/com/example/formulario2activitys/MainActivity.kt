573f8357505ed0387ebefa4ab7ef6c56328d71fd
LuisJose <luisjoserranoef2014@gmail.com>
2023-10-15 22:21:45 +0200

    Primer commit

---
+++ Formulario2Activitys/app/src/main/java/com/example/formulario2activitys/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.formulario2activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
