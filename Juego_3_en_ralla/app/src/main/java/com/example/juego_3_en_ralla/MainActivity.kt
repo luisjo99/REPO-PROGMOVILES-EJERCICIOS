b1efc5d21d5e85122437aea2ef41ede1c971a4f1
LuisJose <luisjoserranoef2014@gmail.com>
2023-10-18 00:51:04 +0200

    Segundo commit

--- Juego_3_en_ralla/app/src/main/java/com/example/juego_3_en_ralla/MainActivity.kt
+++ Juego_3_en_ralla/app/src/main/java/com/example/juego_3_en_ralla/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.juego_3_en_ralla
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.juego_3_en_ralla.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iv11.setImageResource(R.drawable.inicio)
+        binding.iv12.setImageResource(R.drawable.inicio)
+        binding.iv13.setImageResource(R.drawable.inicio)
+        binding.iv21.setImageResource(R.drawable.inicio)
+        binding.iv22.setImageResource(R.drawable.inicio)
+        binding.iv23.setImageResource(R.drawable.inicio)
+        binding.iv31.setImageResource(R.drawable.inicio)
+        binding.iv32.setImageResource(R.drawable.inicio)
+        binding.iv33.setImageResource(R.drawable.inicio)
     }
 }
